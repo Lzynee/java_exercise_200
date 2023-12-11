3054d32ba2fdd2c2bc3e846007abb23dad19153d
Lzynee <raina2zen90@gmail.com>
2023-12-11 20:17:53 +0900

    코드 재작성

--- src/com/exercise/oop1/StudentExe.java
+++
@@ -1,9 +0,0 @@
-package com.exercise.oop1;
-
-public class StudentExe {
-    public static void main(String[] args) {
-        Student s = new Student("홍길동", 1, 1, 100, 60, 76);
-
-        System.out.println(s.info());
-    }
-}
